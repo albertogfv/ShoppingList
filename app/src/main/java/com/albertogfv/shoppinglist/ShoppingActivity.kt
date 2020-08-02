b715de87058a52aeb0f4b36379b0eb50ee44555b
albertogfv <albertogfv@gmail.com>
2020-08-02 09:12:32 -0700

    added shopping Dao, and database

--- app/src/main/java/com/albertogfv/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/albertogfv/shoppinglist/ShoppingActivity.kt
@@ -3,10 +3,10 @@ package com.albertogfv.shoppinglist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shopping)
         
     }
 }
